8145a12daa8f6e72ef64532db3f50b5f8d39ce59
Ludvik <luddmachine@gmail.com>
2020-04-28 16:12:28 +0200

    added bunch of reviews in data

--- frontend/src/data/reviews.js
+++ frontend/src/data/reviews.js
@@ -5,7 +5,7 @@ export const reviewData = [
         reviewText: 'Wow! Denne burgeren er noe av det beste jeg har spist, denne kommer jeg absolutt til å bestille igjen!',
         starValue: 5,
         timeStamp: new Date('Apr 24, 2020, 12:00:00'),
-        menuItemId: 6
+        menuItemId: 1
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export const reviewData = [
         reviewText: 'En fryd å putte i munnen! Kjøttet ble litt tørt dessverre, ellers hadde denne fått full pott.',
         starValue: 4,
         timeStamp: new Date('Apr 23, 2020, 13:00:00'),
-        menuItemId: 4
+        menuItemId: 1
     },
     {
         id: 3,
@@ -28,7 +28,288 @@ export const reviewData = [
         reviewer: 'Pontus Pilatus',
         reviewText: 'Dette var en tankevekker av en burger! ',
         starValue: 4,
+        timeStamp: new Date('Apr 23, 2020, 12:00:00'),
+        menuItemId: 2
+    },
+    {
+        id: 5,
+        reviewer: 'Guri Halvorsen',
+        reviewText: 'Utrolig god på smak!',
+        starValue: 4,
         timeStamp: new Date('Apr 26, 2020, 12:00:00'),
         menuItemId: 3
+    },
+    {
+        id: 6,
+        reviewer: 'Hanna Hannesen',
+        reviewText: 'Helt ok for min del! Varierer litt fra gang til gang.',
+        starValue: 3,
+        timeStamp: new Date('Apr 25, 2020, 12:00:00'),
+        menuItemId: 3
+    },
+    {
+        id: 7,
+        reviewer: 'Hans Hansen',
+        reviewText: 'Svelga nettopp denne burgern, og jeg ble ordentlig godmett. Suveren burger!',
+        starValue: 5,
+        timeStamp: new Date('Apr 27, 2020, 12:00:00'),
+        menuItemId: 4
+    },
+    {
+        id: 8,
+        reviewer: 'Kristian Kristiansen',
+        reviewText: 'Mangler det lille ekstra for full pott!',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 12:00:00'),
+        menuItemId: 4
+    },
+    {
+        id: 9,
+        reviewer: 'Christer Christensen',
+        reviewText: 'Bestilte feil egentlig, men en kjøttfri burger var ikke så aller verst!',
+        starValue: 2,
+        timeStamp: new Date('Apr 23, 2020, 12:00:00'),
+        menuItemId: 4
+    },
+    {
+        id: 10,
+        reviewer: 'Vanessa Senkveld',
+        reviewText: 'Diiiiiigg burger i en casual sjappe!',
+        starValue: 4,
+        timeStamp: new Date('Apr 27, 2020, 11:00:00'),
+        menuItemId: 5
+    },
+    {
+        id: 11,
+        reviewer: 'Selma Selmesen',
+        reviewText: 'Beste burgern i bydelen!',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 5
+    },
+    {
+        id: 12,
+        reviewer: 'Fredrik Fredriksen',
+        reviewText: 'Litt soggy dessverre, håper på en bedre burger neste gang...',
+        starValue: 1,
+        timeStamp: new Date('Apr 24, 2020, 14:00:00'),
+        menuItemId: 5
+    },
+    {
+        id: 13,
+        reviewer: 'Sol Vintersen',
+        reviewText: 'Elsker en burger med litt zing! Denne var heeeelt perfekt for min del!',
+        starValue: 5,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 6
+    },
+    {
+        id: 14,
+        reviewer: 'Fredrik Fredriksen',
+        reviewText: 'Perfekt crisp på utsiden, myk inni!',
+        starValue: 4,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 7
+    },
+    {
+        id: 15,
+        reviewer: 'Karla Karlsvogn',
+        reviewText: 'En perfekt snack ved siden av en god burger!',
+        starValue: 5,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 7
+    },
+    {
+        id: 16,
+        reviewer: 'Sokrates',
+        reviewText: 'Jeg tenker denne kunne vært bedre, men kunne også vært verre!',
+        starValue: 3,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 8
+    },
+    {
+        id: 17,
+        reviewer: 'Pål Pålestikk',
+        reviewText: 'Forventet mer, osten var ikke like smeltet som man vil',
+        starValue: 3,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 8
+    },
+    {
+        id: 18,
+        reviewer: 'Hedvig Uglesen',
+        reviewText: 'Veldig gode! Kunne vært litt mer spicy for min del.',
+        starValue: 4,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 9
+    },
+    {
+        id: 19,
+        reviewer: 'Harald Haraldsen',
+        reviewText: 'En perfekt finisher etter en rågod burger, kunne vært bittelitt mer juicy.',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 9
+    },
+    {
+        id: 20,
+        reviewer: 'Dave East',
+        reviewText: 'Chicken wing with a zing, count me in!',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 9
+    },
+    {
+        id: 21,
+        reviewer: 'Tommy Thomassen',
+        reviewText: 'Mmmm, etter du har spist siste ønsker du bare at du hadde bestilt en porsjon til!',
+        starValue: 5,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 10 
+    },
+    {
+        id: 22,
+        reviewer: 'Fredda Freddesen',
+        reviewText: 'Oioioi, perfekt crisp men samtidig gode og varme!',
+        starValue: 5,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 11
+    },
+    {
+        id: 23,
+        reviewer: 'Linda Lærer',
+        reviewText: 'Litt soggy, men det kan være fordi jeg tok med hjem!',
+        starValue: 4,
+        timeStamp: new Date('Apr 22, 2020, 14:00:00'),
+        menuItemId: 11
+    },
+    {
+        id: 24,
+        reviewer: 'Jonny Johnson',
+        reviewText: 'Klassikern! Denne er viktig å naile, her har de litt å lære.',
+        starValue: 3,
+        timeStamp: new Date('Apr 20, 2020, 14:00:00'),
+        menuItemId: 11
+    },
+    {
+        id: 25,
+        reviewer: 'Julie Jett',
+        reviewText: 'Perfekt salt til potet ratio, meget smakelig.',
+        starValue: 4,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 11
+    },
+    {
+        id: 26,
+        reviewer: 'Klara Kald',
+        reviewText: 'Fries med litt sting i seg! En fin siderett med burgern.',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 12
+    },
+    {
+        id: 27,
+        reviewer: 'Jonny Johnson',
+        reviewText: 'Godt, men kunne vært enda litt bedre.',
+        starValue: 3,
+        timeStamp: new Date('Apr 24, 2020, 14:00:00'),
+        menuItemId: 12
+    },
+    {
+        id: 28,
+        reviewer: 'Sara Santa',
+        reviewText: 'Søtpotet fries er det beste jeg vet! Og her har de noen av de beste.',
+        starValue: 5,
+        timeStamp: new Date('Apr 28, 2020, 14:00:00'),
+        menuItemId: 13
+    },
+    {
+        id: 29,
+        reviewer: 'Juni Hotell',
+        reviewText: 'Fantastisk crisp!',
+        starValue: 4,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 13
+    },
+    {
+        id: 30,
+        reviewer: 'Louis',
+        reviewText: 'Nydelig god på smak.',
+        starValue: 4,
+        timeStamp: new Date('Apr 24, 2020, 14:00:00'),
+        menuItemId: 13
+    },
+    {
+        id: 31,
+        reviewer: 'Platon',
+        reviewText: 'Jeg tenker ja!',
+        starValue: 5,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 14
+    },
+    {
+        id: 32,
+        reviewer: 'Pål Haraldsen',
+        reviewText: 'Digg med litt aioli til søtpotet fries!',
+        starValue: 4,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 15
+    },
+    {
+        id: 33,
+        reviewer: 'Aristoteles',
+        reviewText: 'Jeg tenker nei... Denne ble for undervelmende.',
+        starValue: 1,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 16
+    },
+    {
+        id: 34,
+        reviewer: 'Børre Børresen',
+        reviewText: 'Joa, helt ok!',
+        starValue: 3,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 16
+    },
+    {
+        id: 35,
+        reviewer: 'Roger Federer',
+        reviewText: 'Very nice!',
+        starValue: 4,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 17
+    },
+    {
+        id: 36,
+        reviewer: 'Rafael Nadal',
+        reviewText: 'Muy bien!',
+        starValue: 4,
+        timeStamp: new Date('Apr 25, 2020, 14:00:00'),
+        menuItemId: 17
+    },
+    {
+        id: 37,
+        reviewer: 'Håkon Håkonsen',
+        reviewText: 'Perfekt dip til burgern!',
+        starValue: 5,
+        timeStamp: new Date('Apr 27, 2020, 14:00:00'),
+        menuItemId: 18
+    },
+    {
+        id: 38,
+        reviewer: 'Tom Tommy',
+        reviewText: 'Sier ikke neitakk til en jatakk!',
+        starValue: 5,
+        timeStamp: new Date('Apr 26, 2020, 14:00:00'),
+        menuItemId: 18
+    },
+    {
+        id: 39,
+        reviewer: 'Roy Arne',
+        reviewText: 'Dette er kirsebæret på toppen av kransekaka hvis du skjønner hva jeg mener!',
+        starValue: 5,
+        timeStamp: new Date('Apr 24, 2020, 14:00:00'),
+        menuItemId: 18
     }
+
 ]
